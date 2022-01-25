5bb751ba164e311a8043ebe45c51a9e545a5cfba
Ayoola Taiwo <tayoola13@yahoo.com>
2022-01-25 23:19:47 +0100

    Input field added to middle panel and styled

--- src/components/chatroom/MiddlePanel.vue
+++ src/components/chatroom/MiddlePanel.vue
@@ -3,20 +3,38 @@
     <div v-if="friend" class="friend">
       <div
         class="messagefield"
-        :class="{'friend-style': friend.id===messageObject.user.id}"
+        :class="{ 'friend-style': friend.id === messageObject.user.id }"
         v-for="(messageObject, index) in messagesArray"
         :key="index"
       >
-        <div class="image-message" :class="{'friend-style-inner': friend.id===messageObject.user.id}">
+        <div
+          class="image-message"
+          :class="{ 'friend-style-inner': friend.id === messageObject.user.id }"
+        >
           <user-small-image :image="friend.image" :name="friend.name" />
           <div>
-            <div :class="{'user-background': friend.id !== messageObject.user.id, 'friend-background': friend.id === messageObject.user.id}"><p>{{ messageObject.message }}</p></div>
-            <span class="stamp-font" :class="{'stamp-style': friend.id === messageObject.user.id}">{{ messageObject.timestamp }}</span>
+            <div
+              :class="{
+                'user-background': friend.id !== messageObject.user.id,
+                'friend-background': friend.id === messageObject.user.id,
+              }"
+            >
+              <p>{{ messageObject.message }}</p>
+            </div>
+            <span
+              class="stamp-font"
+              :class="{ 'stamp-style': friend.id === messageObject.user.id }"
+              >{{ messageObject.timestamp }}</span
+            >
           </div>
         </div>
       </div>
     </div>
     <div v-else>Select a chat</div>
+    <div class="input-field">
+      <input type="text" placeholder="Enter your message here" />
+      <div class="submit-button" @click="handleSubmit">send</div>
+    </div>
   </div>
 </template>
 
@@ -25,18 +43,19 @@ import UserSmallImage from "./UserSmallImage.vue";
 export default {
   components: { UserSmallImage },
   name: "MiddlePanel",
-  props: ["friend"],
+  props: ["friend", "user"],
   data() {
     return {
       messagesArray: [
         {
           user: { id: 1, image: "", name: "Ayoola Taiwo" },
-          message: "hello Bill, trust you doing greatsdljndjd,sweowe wwkedoid ewdoewoidew ewoideowiewje nowedoewnwe3do3 o32w3rdnoawlwoe dweoidoweid eowdweoewe",
+          message:
+            "hello Bill, trust you doing greatsdljndjd, sweowe wwkedoid ewdoewoidew ewoideowiewje nowedoewnwe3do3 o32w3rdnoawlwoe dweoidoweid eowdweoewe",
           timestamp: "9h ago",
         },
         {
           user: { id: 2, image: "", name: "Bill Gate" },
-          message: "hi henry, I'm fine",
+          message: "hi henry, I'm fine fdl fdd reoker kerpk",
           timestamp: "9h ago",
         },
       ],
@@ -50,7 +69,10 @@ export default {
 .middle-panel-container {
   background: #f3f6fb;
   border-radius: 15px;
-  padding: 25px;
+  padding: 20px ;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   position: relative;
 }
 .image-message {
@@ -65,29 +87,56 @@ export default {
 .friend-style-inner {
   flex-direction: row-reverse;
 }
-.user-background{
-    background: #fff;
-    color:#525C72;
-    padding: 15px;
-    border-radius: 12px;
-    margin-right: 10px;
-    margin-bottom: 10px;
+.user-background {
+  background: #fff;
+  color: #525c72;
+  padding: 15px;
+  border-radius: 12px;
+  margin-right: 10px;
+  margin-bottom: 10px;
 }
-.friend-background{
-    background: #1A233B;
-    color: #fefefe;
-    padding: 15px;
-    border-radius: 12px;
-    margin-right: 10px;
-    margin-bottom: 10px;
+.friend-background {
+  background: #1a233b;
+  color: #fefefe;
+  padding: 15px;
+  border-radius: 12px;
+  margin-right: 10px;
+  margin-bottom: 10px;
 }
-.stamp-font{
+.stamp-font {
   font-size: 14px;
-  color: #525C70;
+  color: #525c70;
 }
-.stamp-style{
-    float: right;
-    margin-right: 10px;
+.stamp-style {
+  float: right;
+  margin-right: 10px;
 }
+.input-field {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
 
+}
+.input-field input{
+  outline: none;
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+  border: 1px solid;
+  padding: 3px 5px;
+}
+.submit-button{
+  height: 100%;
+  border: 1px solid;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 4px 10px;
+  margin-left: 10px;
+  border-radius: 8px;
+  cursor: pointer;
+}
 </style>
--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,37 +1,42 @@
 <template>
   <div class="chat-container">
     <LeftPanel v-on:converser="passConverser" />
-    <MiddlePanel :friend="converser"/>
+    <MiddlePanel :friend="converser" :user="user" />
     <RightPanel />
   </div>
 </template>
 
 <script>
-import LeftPanel from '../components/chatroom/LeftPanel';
-import MiddlePanel from '../components/chatroom/MiddlePanel';
-import RightPanel from '../components/chatroom/RightPanel';
+import LeftPanel from "../components/chatroom/LeftPanel";
+import MiddlePanel from "../components/chatroom/MiddlePanel";
+import RightPanel from "../components/chatroom/RightPanel";
+
+// import flash_logo from "../../assets/flash_logo.png";
+import passport from "../assets/passport.jpg";
+// import woman_logo from "../../assets/woman.png";
+
 
 export default {
   name: "Chat",
-  components: {LeftPanel, MiddlePanel, RightPanel},
-  data(){
-    return{
-      converser: null
-    }
+  components: { LeftPanel, MiddlePanel, RightPanel },
+  data() {
+    return {
+      converser: null,
+      user: { image: passport, name: "Ayoola Taiwo", id: 1 }
+    };
   },
   methods: {
-    passConverser(e){
-      console.log(e)
+    passConverser(e) {
       this.converser = e;
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
-.chat-container{
+.chat-container {
   display: grid;
-  grid-template-columns:  1fr 3fr 1fr;
+  grid-template-columns: 1fr 3fr 1fr;
   gap: 30px;
   height: 90vh;
 }
